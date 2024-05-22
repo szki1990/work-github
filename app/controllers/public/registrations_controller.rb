31c7cff15034d1a3ac502a45a6733c7847e20d03
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-22 14:31:35 +0000

    [update]parameter

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys[:name])
+  end 
 
+  def after_sign_up_path_for(resource)
+  end 
   # GET /resource/sign_up
   # def new
   #   super
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,11 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  def after_sign_in_path_for(resource)
+  end 
+  
+  def after_sign_out_path_for(resource)
+  end 
 
   # GET /resource/sign_in
   # def new
