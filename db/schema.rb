fcdcb3eee202232b5f102c45f796df6b71cca98f
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-22 05:02:13 +0000

    model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_22_044331) do
+ActiveRecord::Schema.define(version: 2024_05_22_045246) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
